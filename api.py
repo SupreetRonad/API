345aee1969bbca8155bd70afa27a678253e6eb8f
SupreetRonad <62396592+SupreetRonad@users.noreply.github.com>
2021-06-13 19:02:15 +0530

    initial commit

---
+++ api.py
@@ -0,0 +1,19 @@
+# from provider import *
+
+from flask import Flask
+# , jsonify, redirect, url_for, render_template, request
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    # init()
+    # image = "hello"
+    # if request.method=="POST":
+    #     image = request.form['img']
+    #     return render_template("home.html", imgRes = validate(image))
+    # return render_template("home.html")
+    return 'Sent from API'
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
